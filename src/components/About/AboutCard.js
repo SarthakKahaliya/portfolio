9d3fb0e324292fe71d5ce9b3d3e3378c87c34378
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-09-16 19:59:59 -0400

    update about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -21,17 +21,17 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Cooking
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Sarthak</footer>
+          <footer className="blockquote-footer">Sarthak</footer> */}
         </blockquote>
       </Card.Body>
     </Card>
