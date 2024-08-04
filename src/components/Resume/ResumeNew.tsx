19234f89c5c6723b56a5649eaca114249bc6fb7f
Sarthak Kahaliya <sarthakkahaliya@gmail.com>
2024-08-04 12:05:35 -0400

    upgraded

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -7,7 +7,7 @@ import Particle from '../Particle';
 import pdf from '../../Assets/Sarthak_Kahaliya_Resume_2022.pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
 const resumeLink =
   'https://raw.githubusercontent.com/SarthakKahaliya/portfolio/main/src/Assets/Sarthak_Kahaliya_Resume_2022.pdf';
@@ -24,7 +24,12 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: '250px' }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: '250px' }}
+          >
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
@@ -32,7 +37,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} renderTextLayer={false} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
