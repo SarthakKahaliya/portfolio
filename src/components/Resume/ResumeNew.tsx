5fd13aa094f06b6201baff1194d18996770ceb63
Sarthak Kahaliya <sarthakkahaliya@gmail.com>
2024-08-09 15:17:54 -0400

    fixed resume

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import Particle from '../Particle';
-import pdf from '../../Assets/Sarthak_Kahaliya_Resume_2022.pdf';
+import pdf from '../../Assets/Sarthak_Kahaliya_Resume_June_2024.pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
