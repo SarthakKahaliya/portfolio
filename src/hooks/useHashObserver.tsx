27362e61a824b8f024366993fc2d95b3480b6e99
Sarthak Kahaliya <skahaliya@lutron.com>
2024-08-09 00:53:35 -0400

    show #

--- src/hooks/useHashObserver.tsx
+++ src/hooks/useHashObserver.tsx
@@ -2,42 +2,45 @@
 import { useEffect, useState } from 'react';
 
 const useHashObserver = (ids: string[]) => {
-    const [activeHash, setActiveHash] = useState<undefined | string>(undefined);
+  const [activeHash, setActiveHash] = useState<undefined | string>(undefined);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    const id = entry.target.getAttribute('id');
-                    // navigate(`#${id}`, { replace: true });
-                    window.history.replaceState(null, '', ' ');
-                    setActiveHash(`#${id}`);
-                }
-            });
-        }, {
-            threshold: 0.5, // Adjust the threshold as needed
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const id = entry.target.getAttribute('id');
+            // navigate(`#${id}`, { replace: true });
+            // window.history.replaceState(null, '', ' ');
+            setActiveHash(`#${id}`);
+          }
         });
+      },
+      {
+        threshold: 0.5, // Adjust the threshold as needed
+      }
+    );
 
-        ids.forEach(id => {
-            const element = document.getElementById(id);
-            if (element) {
-                observer.observe(element);
-            } else {
-                setActiveHash(undefined);
-            }
-        });
+    ids.forEach((id) => {
+      const element = document.getElementById(id);
+      if (element) {
+        observer.observe(element);
+      } else {
+        setActiveHash(undefined);
+      }
+    });
 
-        return () => {
-            ids.forEach(id => {
-                const element = document.getElementById(id);
-                if (element) {
-                    observer.unobserve(element);
-                }
-            });
-        };
-    }, [ids, setActiveHash]);
+    return () => {
+      ids.forEach((id) => {
+        const element = document.getElementById(id);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+    };
+  }, [ids, setActiveHash]);
 
-    return activeHash;
+  return activeHash;
 };
 
 export default useHashObserver;
